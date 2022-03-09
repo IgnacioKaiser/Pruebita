67f6a9db6ad88e3f452b3d927feefb98b8f1806b
Ignacio Kaiser <kaiserignacio11@gmail.com>
2022-03-09 20:40:23 -0300

    Prueba3

--- src/pruebita/Pruebita.java
+++ src/pruebita/Pruebita.java
@@ -5,6 +5,8 @@
  */
 package pruebita;
 
+import java.util.Scanner;
+
 /**
  *
  * @author kaise
@@ -15,7 +17,24 @@ public class Pruebita {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Hola pelotudo");
+        Scanner leer = new Scanner(System.in);
+        System.out.println("Ingrese su nombre");
+        String nombre = leer.nextLine();
+        System.out.println("Ingrese su género ('M', 'H' u 'O'");
+        String gen = leer.nextLine();
+        switch (gen) {
+            case "M":
+            case "m":
+                System.out.println(nombre+", es usted una ídola");
+                break;
+            case "h":
+            case "H":
+                System.out.println(nombre+", es usted un ídolo");
+                break;
+            default:
+                System.out.println(nombre+", es usted une ídole");
+                break;
+        }
     }
     
 }
